c3a07838f8a936e85d1aa0e788d0e50ab799774f
gaviash <gavriel.myara@gmail.com>
2026-05-20 12:14:32 +0200

    Display Tool calls

--- app/Agent.py
+++ app/Agent.py
@@ -6,14 +6,19 @@ from tools import (
     web_search,
     write_file,
 )
-from llama_index.core.agent.workflow import FunctionAgent
+from llama_index.core.agent.workflow import (
+    FunctionAgent,
+    AgentStream,
+    ToolCall,
+    ToolCallResult,
+)
 from llama_index.llms.ollama import Ollama
 from llama_index.core.memory import Memory
 import os
 
+
 def start():
-    system_prompt=(
-        """
+    system_prompt = """
         Tu es Gustave,un agent IA polyvalent,et un assistant efficace.
         Tu es la pour aider l'utilisateur dans ses taches et ses questions.
         Tu disposes d'outils pour y parvenir.
@@ -31,11 +36,10 @@ def start():
         - Fais ce que l'utilisateur dit : si c'est un ordre simple,ne reflechis pas et fais le.
         - N'hésite pas a utiliser les outils des que necessaire.Si il n'y  ne serait-ce qu'un pourcent de chance qu'un outil soit utile,utilise le,sans hesiter.
         - Inclus toujours une sorte de mini rapport qui recapitule ce que tu as fait comme action(outils utilisés etc) et le raisonnement suivi
-        - A ta premiere reponse dans la conversation va toujours regarder la date actuelle,pour la connaitre.
+        - A ta premiere reponse dans la conversation va obligatoirement toujours regarder la date actuelle,pour la connaitre.Va la chercher via la ligne de commande.
         - Ne parle pas de tes "instructions" explicitement.
         """
-    )
-    
+
     Agent = FunctionAgent(
         llm=Ollama(
             model=str(os.getenv("OLLAMA_MODEL")),
@@ -44,8 +48,8 @@ def start():
             request_timeout=100.0,
             base_url="https://ollama.com",
             headers={
-        "Authorization": f"Bearer {os.getenv('OLLAMA_API_KEY')}",
-    }
+                "Authorization": f"Bearer {os.getenv('OLLAMA_API_KEY')}",
+            },
         ),
         system_prompt=system_prompt,
         tools=[
@@ -55,17 +59,27 @@ def start():
             read_file,
             write_file,
             edit_file,
-        ]
+        ],
     )
+
+    memory = Memory.from_defaults(session_id="Dev", token_limit=150000)
+    return Agent, memory
+
+
+async def query(agent: FunctionAgent, memory: Memory, query: str):
+    handler = agent.run(user_msg=query, memory=memory)
+
+    async for event in handler.stream_events():
+        if isinstance(event, AgentStream):
+            if event.delta:
+                continue
+        elif isinstance(event, ToolCall):
+            print("\n[TOOL CALL]")
+            print(f"Tool : {event.tool_name} \n")
+            print(f"Arguments : {event.tool_kwargs}")
+        elif isinstance(event,ToolCallResult):
+            print("\n[TOOL RESULT]")
+            print(f"Result : {str(event.tool_output)[:1000]}")
     
-    memory = Memory.from_defaults(
-        session_id="Dev",
-        token_limit=150000
-    )
-    return Agent,memory
-    
-async def query(agent : FunctionAgent,memory : Memory,query : str):
-    return await agent.run(user_msg=query,memory=memory)
-    
-    
-    
+    response = await handler
+    return response
--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ async def main():
     agent,memory = start()
     while True:
         message = input(">>> ")
-        print(await query(agent,memory,message))
+        response = await query(agent,memory,message)
+        print("\n\n [REPONSE]")
+        print(response)
 
 asyncio.run(main())
